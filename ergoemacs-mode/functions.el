bdf0c0cca19d8c75330b17d5efa98fb58ea4ddf8
David Capello <davidcapello@gmail.com>
2016-06-10 11:50:18 -0300

    close-current-buffer should just kill the buffer

--- ergoemacs-mode/functions.el
+++ ergoemacs-mode/functions.el
@@ -359,54 +359,5 @@ Emacs buffers are those whose name starts with *."
 (defvar recently-closed-buffers-max 10 "The maximum length for recently-closed-buffers.")
 
 (defun close-current-buffer ()
-"Close the current buffer.
-
-Similar to (kill-buffer (current-buffer)) with the following addition:
-
-• prompt user to save if the buffer has been modified even if the buffer is not associated with a file.
-• make sure the buffer shown after closing is a user buffer.
-• if the buffer is a file, add the path to the list recently-closed-buffers.
-
-A emacs buffer is one who's name starts with *.
-Else it is a user buffer."
- (interactive)
- (let (emacsBuff-p isEmacsBufferAfter)
-   (if (string-match "^*" (buffer-name))
-       (setq emacsBuff-p t)
-     (setq emacsBuff-p nil))
-
-   ;; offer to save buffers that are non-empty and modified, even for non-file visiting buffer. (because kill-buffer does not offer to save buffers that are not associated with files)
-   (when (and (buffer-modified-p)
-              (not emacsBuff-p)
-              (not (string-equal major-mode "dired-mode"))
-              (if (equal (buffer-file-name) nil) 
-                  (if (string-equal "" (save-restriction (widen) (buffer-string))) nil t)
-                t
-                )
-              )
-     (if (y-or-n-p
-            (concat "Buffer " (buffer-name) " modified; Do you want to save?"))
-       (save-buffer)
-       (set-buffer-modified-p nil)))
-
-   ;; save to a list of closed buffer
-   (when (not (equal buffer-file-name nil))
-     (setq recently-closed-buffers
-           (cons (cons (buffer-name) (buffer-file-name)) recently-closed-buffers))
-     (when (> (length recently-closed-buffers) recently-closed-buffers-max)
-           (setq recently-closed-buffers (butlast recently-closed-buffers 1))
-           )
-     )
-
-   ;; close
-   (kill-buffer (current-buffer))
-
-   ;; if emacs buffer, switch to a user buffer
-   (if (string-match "^*" (buffer-name))
-       (setq isEmacsBufferAfter t)
-     (setq isEmacsBufferAfter nil))
-   (when isEmacsBufferAfter
-     (next-user-buffer)
-     )
-   )
- )
+  (interactive)
+  (kill-buffer (current-buffer)))
