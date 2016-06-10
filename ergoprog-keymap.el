e8a8afde97c3e0ca728300ed804ab73342d1c6e8
David Capello <davidcapello@gmail.com>
2016-06-10 11:42:58 -0300

    I've been using <Alt+m Alt+f> for projectile-find-file

--- ergoprog-keymap.el
+++ ergoprog-keymap.el
@@ -40,6 +40,7 @@
 (define-key ergoemacs-keymap (kbd "M-m M-s") 'save-some-buffers)
 (define-key ergoemacs-keymap (kbd "M-m f") 'find-file)
 (define-key ergoemacs-keymap (kbd "M-m d") 'ido-dired)
+(define-key ergoemacs-keymap (kbd "M-m M-f") 'projectile-find-file)
 (define-key ergoemacs-keymap (kbd "M-m h") 'back-to-indentation)
 (define-key ergoemacs-keymap (kbd "M-m t") 'transpose-chars)
 (define-key ergoemacs-keymap (kbd "M-m M-t") 'transpose-words)
